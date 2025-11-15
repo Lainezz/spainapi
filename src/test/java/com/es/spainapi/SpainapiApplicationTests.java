25338e6c91b33ffbe22255d2ee3aa1dcbd892e03
Diego <diegolinaresmojacar@gmail.com>
2025-11-15 17:36:37 +0100

    add tesst

--- src/test/java/com/es/spainapi/SpainapiApplicationTests.java
+++ src/test/java/com/es/spainapi/SpainapiApplicationTests.java
@@ -2,12 +2,19 @@ package com.es.spainapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("dev")
 class SpainapiApplicationTests {
 
 //	@Test
 	//void contextLoads() {
 	//}
+
+    @Test
+    void test1() {
+        assert true;
+    }
 
 }
